d25d046c092c60cba90d89a42a9f346b9089e7ae
AJAlilano <alan.alilano@boom.camp>
2019-08-13 17:47:00 +0800

    Subject: Viewing of Cohort Component
    Body: Displayed the Cohort Component
    
    Task:
    Sub Task:

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const massive = require('massive');
+const cors = require("cors");
+const bodyParser = require('body-parser');
 
 massive({
     host: 'localhost',
@@ -9,11 +11,21 @@ massive({
     password: 'handraiser',
 })
 .then(db => {
+    const cohorts = require('./controller/cohorts.js')
     const PORT = 3001;
     const app = express();
 
     app.set('db', db);
+    app.use(cors());
     app.use(express.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    app.get('/api/cohorts/', cohorts.getAll);
+    app.get('/api/cohorts/mentor/:id', cohorts.getByMentorID);
+    app.get('/api/cohorts/:id', cohorts.deleteCohort);
+
+    app.post('/api/cohorts/mentor/:id/add', cohorts.addCohort);
 
     app.listen(PORT, () => {
         console.log(`Running on port ${PORT}`)
