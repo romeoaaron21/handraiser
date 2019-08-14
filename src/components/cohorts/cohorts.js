22d6481d6c01297e4565a4312bb9dd9aab704013
AJAlilano <alan.alilano@boom.camp>
2019-08-14 17:30:22 +0800

    Display of Cohorts, with Enrolling and Leaving also Finished
    Body: Differenciating Enrolled Cohorts and non-enrolled cohorts, Enrolling and leaving cohorts done
    
    Task: Display Cohorts with Enroll and Leave
    Sub Task:

--- src/components/cohorts/cohorts.js
+++ src/components/cohorts/cohorts.js
@@ -12,8 +12,10 @@ import Typography from '@material-ui/core/Typography';
 
 import AddIcon from '@material-ui/icons/Add';
 
-import AddClass from './modals/mentor/add.js';
-import DeleteClass from './modals/mentor/delete.js';
+import AddClass from './modals/mentor/add';
+import DeleteClass from './modals/mentor/delete';
+import EnrollToClass from './modals/student/enroll';
+import LeaveClass from './modals/student/leave'
 
 const styles = theme => ({
   container: {
@@ -61,23 +63,26 @@ class Cohorts extends React.Component{
     super();
 
     this.state = {
-      privilege: 'mentor',
-      id: 1,
+      privilege: 'student',
+      id: 3,
       cohorts: [],
+      member: [],
       add: false,
       delete: false,
-      tobeDeleted: ''
+      enroll: false,
+      leave: false,
+      selected: ''
     }
   }
 
   componentDidMount(){
     if(this.state.privilege === 'mentor'){
-      axios(`http://localhost:3001/api/cohorts/mentor/${this.state.id}`, {
+      axios(`http://localhost:3001/api/mentor/${this.state.id}/cohorts/`, {
         method: 'get'
       }).then((res) => {
         console.log(res.data.cohorts)
         this.setState({
-          cohorts: res.data.cohorts
+          cohorts: res.data.cohorts.reverse()
         })
       }).catch((err) => {
 
@@ -93,6 +98,14 @@ class Cohorts extends React.Component{
       }).catch((err) => {
         
       })
+      axios(`http://localhost:3001/api/student/${this.state.id}/cohorts/`, {
+        method: 'get'
+      }).then((res) => {
+        console.log(res.data.member)
+        this.setState({
+          member: res.data.member
+        })
+      })
     }
   }
 
@@ -105,14 +118,40 @@ class Cohorts extends React.Component{
   openDelete = (e) => {
     this.setState({
       delete: true,
-      tobeDeleted: e.currentTarget.getAttribute('id')
+      selected: e.currentTarget.getAttribute('id')
+    })
+  }
+
+  openEnroll = (e) => {
+    let id = e.currentTarget.getAttribute('id');
+    let array = e.currentTarget.getAttribute('name').split(",");
+    let check = array.find(name => name === 'goToClass');
+    console.log(id)
+
+    if(check){
+      console.log('class')
+    }else{
+      this.setState({
+        enroll: true,
+        selected: e.currentTarget.getAttribute('id')
+      })
+      console.log('enroll')
+    }
+  }
+
+  openLeave = (e) => {
+    this.setState({
+      leave: true,
+      selected: e.currentTarget.getAttribute('id')
     })
   }
 
   closeModal = () => {
     this.setState({
       add: false,
-      delete: false
+      delete: false,
+      enroll: false,
+      leave: false,
     })
   }
 
@@ -137,119 +176,124 @@ class Cohorts extends React.Component{
       this.componentDidMount();
     })
   }
+
+  enroll = (id, password) => {
+    let studentid = this.state.id;
+    const state = {
+      studentid, password
+    }
+    axios(`http://localhost:3001/api/cohorts/${id}/students`, {
+      method: 'post',
+      data: state
+    }).then(() => {
+      this.componentDidMount();
+    })
+  }
+
+  leave = (id) => {
+    // console.log(id, this.state.id)
+    
+    axios(`http://localhost:3001/api/cohorts/${id}/students/${this.state.id}`, {
+      method: 'get',
+    }).then(() => {
+      this.componentDidMount();
+    })
+  }
   
   render(){
-    const { classes } = this.props
+    const { classes } = this.props;
     return(
       <Container maxWidth="md" className={classes.container}>
-        { this.state.privilege === 'mentor' ? 
-          <React.Fragment>
-            <Card className={classes.card}>
-              <CardActionArea className={classes.addCardContainer} onClick={this.openAdd}>
-                <CardContent >
-                  <div className={classes.add}>
-                    <AddIcon />
-                  </div>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-            {this.state.cohorts.map(cohort => (
-              <Card className={classes.card} key={cohort.id}>
-                <CardActionArea className={classes.cardContainer}>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {cohort.name}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      Password: {cohort.password}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      Class Members: 
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions className={classes.buttonContainer}>
-                  <Button size="small" color="primary" id={cohort.id} onClick={this.openDelete}>
-                    Delete
-                  </Button>
-                  <Button size="small" color="primary">
-                    Students
-                  </Button>
-                </CardActions>
-              </Card>
-            ))}
-          </React.Fragment>
-        :
-          <React.Fragment>
-            {this.state.cohorts.map(cohort => (
-              <Card className={classes.card} key={cohort.id}>
-                <CardActionArea className={classes.cardContainer}>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {cohort.name}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      Mentor: 
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      Class Members: 
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions className={classes.buttonContainer}>
-                  <Button size="small" color="primary">
-                    Delete
-                  </Button>
-                  <Button size="small" color="primary">
-                    Students
-                  </Button>
-                </CardActions>
-              </Card>
-            ))}
-            <Card className={classes.card}>
-              <CardActionArea className={classes.cardContainer}>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Class Title asdasdasdasdsa
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Mentor
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className={classes.buttonContainer}>
-                <Button size="small" color="primary">
-                  Leave
-                </Button>
-                
-              </CardActions>
-            </Card>
-            <Card className={classes.card}>
-              <CardActionArea className={classes.wholeCardContainer}>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Class Title
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Mentor (class not yet joined)
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-            <Card className={classes.card}>
-              <CardActionArea className={classes.wholeCardContainer}>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Class Title
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Mentor (class not yet joined)
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </React.Fragment>
-        }
+      { this.state.privilege === 'mentor' ? 
+        <React.Fragment>
+          <Card className={classes.card}>
+            <CardActionArea className={classes.addCardContainer} onClick={this.openAdd}>
+              <CardContent >
+                <div className={classes.add}>
+                  <AddIcon />
+                </div>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        { this.state.cohorts.map(cohort => (
+          <Card className={classes.card} key={cohort.id}>
+            <CardActionArea className={classes.cardContainer}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {cohort.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Password: {cohort.password}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Class Members: 
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.buttonContainer}>
+              <Button size="small" color="primary" id={cohort.id} onClick={this.openDelete}>
+                Delete
+              </Button>
+              <Button size="small" color="primary">
+                Students
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+        </React.Fragment>
+      :
+        <React.Fragment>
+      { this.state.cohorts.map(cohort => { 
+        return (
+          <Card className={classes.card} key={cohort.id}>
+            <CardActionArea
+            id={cohort.id}
+            onClick={this.openEnroll}
+            name=
+            { this.state.member.map(member => {
+              if(member.cohortid === cohort.id){ 
+                return 'goToClass'
+              }else{
+                return 'enroll'
+              }
+            })}
+            className=
+            { this.state.member.map(member => {
+              if(member.cohortid === cohort.id){ 
+                return classes.cardContainer
+              }else{
+                return classes.wholeCardContainer
+              }
+            })}
+            >
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {cohort.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Mentor: {cohort.firstname+' '+cohort.lastname}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Class Members: 
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+      { this.state.member.map(member => {
+        if(member.cohortid === cohort.id){
+          return (
+            <CardActions className={classes.buttonContainer} key={member.id}>
+              <Button size="small" color="primary" id={cohort.id} onClick={this.openLeave}>
+                Leave
+              </Button>
+            </CardActions>
+          )
+        } 
+      })}
+          </Card>
+        )
+      })}
+        </React.Fragment>
+      }
         <AddClass 
           open={this.state.add}
           close={this.closeModal}
@@ -258,8 +302,20 @@ class Cohorts extends React.Component{
         <DeleteClass 
           open={this.state.delete}
           close={this.closeModal}
-          id={this.state.tobeDeleted}
+          id={this.state.selected}
           delete={this.delete}
+        />
+        <EnrollToClass 
+          open={this.state.enroll}
+          close={this.closeModal}
+          id={this.state.selected}
+          enroll={this.enroll}
+        />
+        <LeaveClass 
+          open={this.state.leave}
+          close={this.closeModal}
+          id={this.state.selected}
+          leave={this.leave}
         />
       </Container>
     )
