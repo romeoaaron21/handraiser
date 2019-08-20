c9df37b121c5b640a224c9d88e56ce30a86fe4e0
Alan John Alilano <alan.alilano@boom.camp>
2019-08-20 13:46:41 +0800

    Add images in every card
    Modified design to fit images
    
    Cohorts

--- src/components/cohorts/cards/student.js
+++ src/components/cohorts/cards/student.js
@@ -4,31 +4,47 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   card: {
-    height: 200,
+    height: 275,
     width: 300,
     margin: theme.spacing(2)
   },
   wholeCardContainer: {
     height: '100%',
+    width: 300,
     display: 'flex',
     justifyContent: 'flex-start',
-    alignItems: 'center'
+    alignItems: 'flex-start',
+    flexDirection: 'column'
   },
   cardContainer: {
-    height: '75%',
+    height: 225,
+    width: 300,
     display: 'flex',
     justifyContent: 'flex-start',
-    alignItems: 'center'
+    alignItems: 'flex-start',
+    flexDirection: 'column'
+  },
+  cardContent: {
+    height: '55%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexDirection: 'column'
   },
   buttonContainer: {
-    height: '25%',
+    height: 50,
     paddingTop: 0,
     paddingBottom: 0
+  },
+  media: {
+    height: '45%',
+    width: 300
   },
 })
 
@@ -70,7 +86,12 @@ class StudentClassCards extends React.Component {
               classes.wholeCardContainer
           }
           >
-            <CardContent>
+            <CardMedia
+              className={classes.media}
+              image={"https://source.unsplash.com/1600x900?classroom?sig="+Math.random()}
+              title={cohort.name}
+            />
+            <CardContent className={classes.cardContent}>
               <Typography gutterBottom variant="h5" component="h2">
                 {cohort.name}
               </Typography>
