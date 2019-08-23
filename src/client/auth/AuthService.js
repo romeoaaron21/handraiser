df07b20a0595d701c8930dfb921d4403ea8d71a0
Sid Bercasio <sidney.bercasio@boom.camp>
2019-08-23 15:24:54 +0800

    fix jwt decode error
    fixed errors for redirecting if user is not logged in
    
    fix errors

--- src/client/auth/AuthService.js
+++ src/client/auth/AuthService.js
@@ -1,8 +1,7 @@
 import decode from "jwt-decode";
-import api from '../services/fetchApi';
+import api from "../services/fetchApi";
 
 export default class AuthService {
-
   loggedIn = () => {
     const token = this.getToken();
     return !!token;
@@ -10,24 +9,40 @@ export default class AuthService {
 
   logout = () => {
     localStorage.removeItem("id_token");
-    window.location.href = '/sign-in';
+    window.location.href = "/sign-in";
   };
 
   getConfirm = () => {
-    let answer = decode(this.getToken());
-    return answer;
+    try {
+      let answer = decode(this.getToken());
+      return answer;
+    } catch (err) {
+      return (window.location.href = "/");
+    }
   };
 
   getToken = () => {
-    return localStorage.getItem("id_token");
+    try {
+      return localStorage.getItem("id_token");
+    } catch (err) {
+      return (window.location.href = "/");
+    }
   };
 
   getDecodedToken = () => {
-    return decode(localStorage.getItem("id_token"));
+    try {
+      return decode(localStorage.getItem("id_token"));
+    } catch (err) {
+      return (window.location.href = "/");
+    }
   };
 
   getFetchedTokenAPI = () => {
-    let user = decode(localStorage.getItem("id_token"));
-    return api.fetch(`/api/users/${user.sub}`, 'get')
-  }
+    try {
+      let user = decode(localStorage.getItem("id_token"));
+      return api.fetch(`/api/users/${user.sub}`, "get");
+    } catch (err) {
+      return (window.location.href = "/");
+    }
+  };
 }
